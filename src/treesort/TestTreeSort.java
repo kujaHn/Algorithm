21c969c6dbd46dd55b7789ad4632bc84bdee7433
kujaHn <72388950+kujaHn@users.noreply.github.com>
2021-04-03 01:01:23 +0900

    알고리즘
    계수정렬 추가

--- src/treesort/TestTreeSort.java
+++ src/treesort/TestTreeSort.java
@@ -5,11 +5,16 @@ import java.util.Arrays;
 public class TestTreeSort {
     public static void main(String[] args) {
         int[] array = {7, 6, 5, 8, 3, 5, 9, 1, 6};
+        int[] array2 = {1, 3, 2, 4, 3, 2, 5, 3, 1, 2, 3, 4, 4, 3, 5, 1, 2, 3, 5, 2, 3, 1, 4, 3, 5, 1, 2, 1, 1, 1};
 
-        SortLogic sortLogic = new SortLogic();
+        TreeSortLogic treeSortLogic = new TreeSortLogic();
         //1. 힙 구조 정렬
-        int[] heapSort = sortLogic.heapSort(array);
+        int[] heapSort = treeSortLogic.heapSort(array);
         System.out.println(Arrays.toString(heapSort));
+
+        //2. 계수 정렬
+        int[] countSort = treeSortLogic.countSort(array2);
+        System.out.println(Arrays.toString(countSort));
     }
 
 
